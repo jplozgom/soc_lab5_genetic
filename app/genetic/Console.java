3581bc6401c60073c792b8526116594e3f0b5237
Juan Pablo Lozano Gómez <jplozgom@gmail.com>
2020-04-11 13:05:25 -0700

    file reading

--- app/genetic/Console.java
+++ app/genetic/Console.java
@@ -3,14 +3,16 @@ package genetic;
 import genetic.Parsers.QosDataParser;
 import genetic.Parsers.QosDataParserDocx;
 
+import java.io.File;
+
 public class Console {
 
     public static void main(String[] args) {
 
         //1. Load Qos Data Parser
         QosDataParser qosDataParser = new QosDataParserDocx();
-        qosDataParser.setFileName("/../data/Lab Sample Input.docx");
-
+        String path = new File("").getAbsolutePath();
+        qosDataParser.setFileName(path + "/data/Lab Sample Input.docx");
         qosDataParser.parseDataFromFile();
     }
 }
